871e45e067fb53507fbf1e1f6217d3ecb72a037c
EZZZT <77084742+EZZZT@users.noreply.github.com>
2021-01-07 11:36:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,83 +1,170 @@
 'use strict';
 
-let balance = 0
+window.onload = function() {
+  var savegame = JSON.parse(localStorage.getItem("save"));
+  if (typeof savegame.balance !== "undefined") {
+    balance = savegame.balance;
+    document.getElementById('balance').innerHTML = balance;
+  }
+  if (typeof savegame.shops !== "undefined") {
+    shops = savegame.shops; 
+    document.getElementById('shops').innerHTML = shops;
+  }
+  if (typeof savegame.nextCostShop !== "undefined") {
+    nextCostShop = savegame.nextCostShop;
+    document.getElementById('shopCost').innerHTML = nextCostShop;
+  } 
+  if (typeof savegame.businesses!== "undefined") {
+    businesses = savegame.businesses;
+    document.getElementById('businesses').innerHTML = businesses;  
+  }
+  if (typeof savegame.nextCostBusiness !== "undefined") {
+    nextCostBusiness = savegame.nextCostBusiness;
+    document.getElementById('businessCost').innerHTML = nextCostBusiness;
+  }  
+  if (typeof savegame.conglomerates !== "undefined") {
+    conglomerates = savegame.conglomerates;
+    document.getElementById('conglomerates').innerHTML = conglomerates;
+  }
+  if (typeof savegame.nextCostConglomerate !== "undefined") {
+    nextCostConglomerate = savegame.nextCostConglomerate;
+    document.getElementById('conglomerateCost').innerHTML = nextCostConglomerate;
+  }
+  if (typeof savegame.governments !== "undefined") {
+    governments = savegame.governments;
+    document.getElementById('governments').innerHTML = governments;  
+  }
+  if (typeof savegame.nextCostGovernment !== "undefined") {
+    nextCostGovernment = savegame.nextCostGovernment;
+    document.getElementById('governmentCost').innerHTML = nextCostGovernment;  
+  }
+}
+
+var balance=0;
+var shops;
+var nextCostShop;
+var businesses;
+var nextCostBusiness;
+var conglomerates;
+var nextCostConglomerate;
+var governments;
+var nextCostGovernment;
+var resetModal = document.getElementById("resetModal");
+var resetButton = document.getElementById("resetButton");
+var resetClose = document.getElementsByClassName("close")[0];
+var cancel = document.getElementById('cancel')
 
 const addBalance = number => {
   balance = balance + number
   document.getElementById('balance').innerHTML = balance;
 }
 
-let workers = 0
+shops = 0
 
-const buyWorker = () => {
-    let workerCost = Math.floor(10 * Math.pow(1.2,workers));     //works out the cost of this worker
-    if(balance >= workerCost){                                   //checks that the player can afford the worker
-        workers = workers + 1;                                   //increases number of workers
-    	balance = balance - workerCost;                          //removes the balance spent
-        document.getElementById('workers').innerHTML = workers;  //updates the number of workers for the user
+const buyShop = () => {
+    var shopCost = Math.floor(10 * Math.pow(1.2,shops));     //works out the cost of this worker
+    if(balance >= shopCost){                                   //checks that the player can afford the worker
+        shops = shops + 1;                                   //increases number of workers
+    	balance = balance - shopCost;                          //removes the balance spent
+        document.getElementById('shops').innerHTML = shops;  //updates the number of workers for the user
         document.getElementById('balance').innerHTML = balance;  //updates the balance for the user
     };
-    let nextCostWorker = Math.floor(10 * Math.pow(1.2,workers));       //works out the cost of the next worker
-    document.getElementById('workerCost').innerHTML = nextCostWorker;  //updates the worker cost for the user
+     nextCostShop = Math.floor(10 * Math.pow(1.2,shops));       //works out the cost of the next worker
+    document.getElementById('shopCost').innerHTML = nextCostShop;  //updates the worker cost for the user
 };
 
-let factories = 0
+businesses = 0
 
-const buyFactory = () => {
-    let factoryCost = Math.floor(100 * Math.pow(1.2,factories));   
-    if(balance >= factoryCost){                                  
-        factories = factories + 1;                                  
-    	balance = balance - factoryCost;                          
-        document.getElementById('factories').innerHTML = factories;  
+const buyBusiness = () => {
+    var businessCost = Math.floor(100 * Math.pow(1.2,businesses));   
+    if(balance >= businessCost){                                  
+        businesses = businesses + 1;                                  
+    	balance = balance - businessCost;                          
+        document.getElementById('businesses').innerHTML = businesses;  
         document.getElementById('balance').innerHTML = balance;  
     };
-    let nextCostFactory = Math.floor(100 * Math.pow(1.2,factories));       
-    document.getElementById('factoryCost').innerHTML = nextCostFactory;  
+    nextCostBusiness = Math.floor(100 * Math.pow(1.2,businesses));       
+    document.getElementById('businessCost').innerHTML = nextCostBusiness;  
 };
 
-let conglomerates = 0
+conglomerates = 0
 
 const buyConglomerate = () => {
-    let conglomerateCost = Math.floor(1000 * Math.pow(1.2,conglomerates));   
+    var conglomerateCost = Math.floor(1000 * Math.pow(1.2,conglomerates));   
     if(balance >= conglomerateCost){                                  
         conglomerates = conglomerates + 1;                                  
     	balance = balance - conglomerateCost;                          
         document.getElementById('conglomerates').innerHTML = conglomerates;  
         document.getElementById('balance').innerHTML = balance;  
     };
-    let nextCostConglomerate = Math.floor(1000 * Math.pow(1.2,conglomerates));       
+    nextCostConglomerate = Math.floor(1000 * Math.pow(1.2,conglomerates));       
     document.getElementById('conglomerateCost').innerHTML = nextCostConglomerate;  
 };
 
-let governments = 0 
+governments = 0 
 
 const buyGovernment = () => {
-    let governmentCost = Math.floor(10000 * Math.pow(1.2,governments));   
+    var governmentCost = Math.floor(10000 * Math.pow(1.2,governments));   
     if(balance >= governmentCost){                                  
         governments = governments + 1;                                  
     	balance = balance - governmentCost;                          
         document.getElementById('governments').innerHTML = governments;  
         document.getElementById('balance').innerHTML = balance;  
     };
-    let nextCostGovernment = Math.floor(10000 * Math.pow(1.2,governments));       
+    nextCostGovernment = Math.floor(10000 * Math.pow(1.2,governments));       
     document.getElementById('governmentCost').innerHTML = nextCostGovernment;  
 };
 
 const save = () => {
-  let saveObject = {
+  var saveObject = {
    balance: balance,
-   shops: workers,
-   businesses: factories,
+   shops: shops,
+   nextCostShop: nextCostShop,
+   businesses: businesses,
+   nextCostBusiness: nextCostBusiness,
    conglomerates: conglomerates,
-   governments: governments
+   nextCostConglomerate: nextCostConglomerate,
+   governments: governments,
+   nextCostGovernment: nextCostGovernment
   }
   localStorage.setItem("save",JSON.stringify(saveObject));
   console.log(saveObject)
 }
 
+const reset = () => {
+  localStorage.removeItem("save")
+  balance = 0
+  shops = 0
+  document.getElementById('shops').innerHTML = shops; 
+  document.getElementById('shopCost').innerHTML = '10'
+  businesses = 0
+  document.getElementById('businesses').innerHTML = businesses; 
+  document.getElementById('businessCost').innerHTML = '100'
+  conglomerates = 0
+  document.getElementById('conglomerates').innerHTML = conglomerates; 
+  document.getElementById('conglomerateCost').innerHTML = '1000'
+  governments = 0
+  document.getElementById('governments').innerHTML = governments; 
+  document.getElementById('governmentCost').innerHTML = '10000'
+  resetModal.style.display = "none";
+}
+
+resetButton.onclick = function() {
+  resetModal.style.display = "block";
+}
+
+
+resetClose.onclick = function() {
+  resetModal.style.display = "none";
+}
+
+cancel.onclick = function() {
+  resetModal.style.display = "none";
+}
+
 window.setInterval(function(){
-  addBalance(workers)
-  addBalance(factories * 10)
+  addBalance(shops)
+  addBalance(businesses * 10)
   addBalance(conglomerates * 100)
   addBalance(governments * 1000)
 }, 1000);
@@ -85,4 +172,5 @@ window.setInterval(function(){
 window.setInterval(function(){
   save()
  }, 20000)
+
 
